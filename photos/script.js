c6f28c7d7d07910596017bea97d3665f4576f44d
vanshsukhiyaji07 <6494vanshsukhiyaji@gmail.com>
2024-10-08 20:42:37 +0530

    Update script.js

--- photos/script.js
+++ photos/script.js
@@ -1,37 +1,61 @@
-let button = document.getElementById("fetch-btn")
-button.addEventListener("click", ButtonClicked);
+// let button = document.getElementById("fetch-btn")
+// button.addEventListener("click", ButtonClicked);
 
-function ButtonClicked() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
-    xhr.onprogress = function () {
-        console.log("On progress");
-    };
-    xhr.onload = function () {
-        console.log(this.status);
-        if (this.status == 200 || this.status == 201) {
-            const photos = JSON.parse(this.responseText);
-            console.log(photos);
-            displayPhotos(photos);
-        } else {
-            console.log("Page Not found");
-        }
-    };
-    console.log(this.status);
-    xhr.send();
-}
+// function ButtonClicked() {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
+//     xhr.onprogress = function () {
+//         console.log("On progress");
+//     };
+//     xhr.onload = function () {
+//         console.log(this.status);
+//         if (this.status == 200 || this.status == 201) {
+//             const photos = JSON.parse(this.responseText);
+//             console.log(photos);
+//             displayPhotos(photos);
+//         } else {
+//             console.log("Page Not found");
+//         }
+//     };
+//     console.log(this.status);
+//     xhr.send();
+// }
 
-function displayPhotos(photos) {
-    const displayData = document.getElementById("display-data");
-    photos.forEach((photo) => {
-        let img = document.createElement("img");
-        img.classList.add("photo");
-        img.style.margin = "10px";
-        img.style.padding = "10px";
-        img.style.border = "1px solid black";
-        img.style.width = "200px";
-        img.style.display = "block";
-        img.src = photo.url;
-        displayData.appendChild(img);
-    });
-}
+// function displayPhotos(photos) {
+//     const displayData = document.getElementById("display-data");
+//     photos.forEach((photo) => {
+//         let img = document.createElement("img");
+//         img.classList.add("photo");
+//         img.style.margin = "10px";
+//         img.style.padding = "10px";
+//         img.style.border = "1px solid black";
+//         img.style.width = "200px";
+//         img.style.display = "block";
+//         img.src = photo.url;
+//         displayData.appendChild(img);
+//     });
+// }
+
+let xhr = new XMLHttpRequest();
+
+const btn = document.getElementById("fetch-btn");
+const contant = document.getElementById("display-data");
+
+btn.addEventListener("click", () => {
+    fetch("https://jsonplaceholder.typicode.com/albums").then((responce) => {
+        return responce.json();
+    }).then((photos) => {
+        const displayData = document.getElementById("display-data");
+        photos.forEach((photo) => {
+            let img = document.createElement("img");
+            img.classList.add("photo");
+            img.style.margin = "10px";
+            img.style.padding = "10px";
+            img.style.border = "1px solid black";
+            img.style.width = "200px";
+            img.style.display = "block";
+            img.src = photo.url;
+            displayData.appendChild(img);
+        })
+    })
+})
